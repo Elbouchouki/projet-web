02054aa9f41128ddc1ed8bef3441305676c6e111
Elbouchouki <elbouchoukigamer@gmail.com>
2022-05-27 01:58:10 +0100

    article paggination fix

--- client/src/js/components/allArticles.js
+++ client/src/js/components/allArticles.js
@@ -12,19 +12,33 @@ export default class AllArticles {
     const articles = await getArticles();
     this.articles = articles;
   }
-
+  async reRenderPagination() {
+    var htmlPagination = /*HTML*/ ` 
+      <div>
+        Showing <span class="font-semibold text-gray-900 dark:text-white">${Math.min(
+          AllArticles.articlesCount * 10,
+          this.articles.length
+        )}</span> of <span class="font-semibold text-gray-900 dark:text-white">${
+      this.articles.length
+    }</span> Articles
+      </div>`;
+    return htmlPagination;
+  }
   async reRender() {
     let htmlArticles = "";
-
-    for (
-      var i = AllArticles.articlesCount;
-      i < AllArticles.articlesCount * 10;
-      i++
-    ) {
+    console.log(AllArticles.articlesCount);
+    let max = (AllArticles.articlesCount + 1) * 10;
+    if ((AllArticles.articlesCount + 1) * 10 > this.articles.length) {
+      max = this.articles.length;
+      $("#all-articles-pagination-button").attr("disabled", true);
+      $("#all-articles-pagination-button").addClass("cursor-not-allowed");
+    }
+    for (var i = AllArticles.articlesCount * 10 || 1; i < max; i++) {
       htmlArticles += await new ArticleCard({
         article: this.articles[i],
       }).render();
     }
+    AllArticles.articlesCount += 1;
     return htmlArticles;
   }
 
@@ -32,11 +46,30 @@ export default class AllArticles {
     await this.fetchArticles();
     return /*html*/ `
     <div class="grid mt-10 grid-cols-1 gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3">
-        <div  class="my-4 col-span-full	 text-2xl font-bold dark:text-white">Articles</div>
-        <div id="AllArticlesGridContainer"  class="my-4 col-span-full grid mt-10 grid-cols-1 gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3">
-         ${await this.reRender()}
+        <div  class="my-4 col-span-full	 text-2xl font-bold dark:text-white">
+          Articles
         </div>
-
+        <div id="AllArticlesGridContainer"  class="my-4 col-span-full grid mt-10 grid-cols-1 gap-4 place-items-center md:grid-cols-2 lg:grid-cols-3">
+          ${await this.reRender()}
+        </div>
+        <div class="mb-5 col-span-full flex flex-col items-center">
+          <span id="all-articles-pagination" class="text-sm text-gray-700 dark:text-gray-400">
+            ${await this.reRenderPagination()}
+          </span>
+          <div class="inline-flex mt-2 xs:mt-0">
+              <button id="all-articles-pagination-button" class="py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                  Show More
+              </button>
+          </div>
+        </div>
     </div>`;
   }
 }
+
+$(document).on("click", "#all-articles-pagination-button", async () => {
+  const newHTML = await AllArticlesComponent.reRender();
+  $("#AllArticlesGridContainer").append($(newHTML));
+  const newPagination = await AllArticlesComponent.reRenderPagination();
+  $("#all-articles-pagination").html($(newPagination));
+  // console.log($("all-articles-pagination"));
+});
--- client/src/js/components/articleCard.js
+++ client/src/js/components/articleCard.js
@@ -14,11 +14,11 @@ export default class ArticleCard {
         <div class="p-5">
             <a href="#">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">${
-                  this.article.titre
+                  this.article?.titre
                 }</h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">${
-              this.article.contenu.slice(0, 150) + "..."
+              this.article?.contenu.slice(0, 150) + "..."
             }</p>
             <a href="#" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Read more
